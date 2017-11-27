722d93e6e9d50fb8331fed2d7957356405c2964c
Sundeep Agarwal <learnbyexample.net@gmail.com>
2017-11-27 12:22:06 +0530

    removed unnecessary comments

--- exercise_files/q6c_max_nested.py
+++ exercise_files/q6c_max_nested.py
@@ -18,25 +18,3 @@ assert max_nested_braces('{{a+2}*{{b+{c*d}}+e*d}}') == 4
 assert max_nested_braces('{{a+2}*{{b}+{c*d}}+e*d}}') == -1
 
 print('all tests passed')
-
-
-#### alternate using regular expressions
-
-#import re
-#
-#def max_nested_braces(expr):
-#    count = 0
-#    while True:
-#        expr_sub = re.sub(r'\{[^{}]*\}', '', expr)
-#        if expr_sub == expr:
-#            break
-#        count += 1
-#        expr = expr_sub
-#
-#    if re.search(r'[{}]', expr):
-#        return -1
-#    return count
-
-# for bonus, use + instead of *
-#assert max_nested_braces('a*b+{}') == -1
-#assert max_nested_braces('a*{b+{}+c*{e*3.14}}') == -1
