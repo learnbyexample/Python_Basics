fec6799178584fca4227f98e75f3573c77c6dc8d
Sundeep Agarwal <learnbyexample.net@gmail.com>
2018-02-20 18:17:33 +0530

    testing file for returning number exercise

---
+++ exercise_files/q2d_to_num.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def num(ip):
+    pass
+
+assert num(3) == 3
+assert num(0x1f) == 31
+assert num(0b101) == 5
+assert num(0o10) == 8
+assert num(3.32) == 3.32
+assert num('123') == 123
+assert num('-78') == -78
+assert num(" 42  \n ") == 42
+assert num('3.14') == 3.14
+assert num('3.982e5') == 398200.0
+s = '56'
+assert num(s) + 44 == 100
+s = '8' * 10
+assert num(s) == 8888888888
+
+try:
+    assert num('foo')
+except ValueError as e:
+    assert str(e) == 'could not convert string to int or float'
+
+try:
+    assert num(['1', '2.3'])
+except TypeError as e:
+    assert str(e) == 'provide only string input'
+
+print('all tests passed')
