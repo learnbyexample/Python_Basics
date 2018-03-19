cecca579bd8ebddbc17252d8ce2f8ab3450abdda
Sundeep Agarwal <learnbyexample.net@gmail.com>
2018-03-19 16:53:50 +0530

    added another test case

--- exercise_solutions/q2c_str_same_letters.py
+++ exercise_solutions/q2c_str_same_letters.py
@@ -5,6 +5,7 @@ def str_anagram(s1, s2):
 
 assert str_anagram('god', 'Dog')
 assert str_anagram('beat', 'abet')
+assert str_anagram('Tap', 'paT')
 assert not str_anagram('beat', 'table')
 assert not str_anagram('seat', 'teal')
 
