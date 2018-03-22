891874f47ded07a8f8edc6ecb343d91ba921fe33
Sundeep Agarwal <learnbyexample.net@gmail.com>
2018-03-22 10:42:06 +0530

    simplified solution

--- exercise_solutions/q5a_col_sum.py
+++ exercise_solutions/q5a_col_sum.py
@@ -3,12 +3,7 @@
 with open('f1.txt', 'r', encoding='ascii') as f:
     total = 0
     for line in f:
-        num = int(line) if type(line) == int else float(line)
-        #try:
-        #    num = int(line)
-        #except ValueError:
-        #    num = float(line)
-        total += num
+        total += float(line)
 
     assert total == 10485.14
 
